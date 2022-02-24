a81201a106c8afaf48ccd92c7a999fdb0681bc6c
Administrator <admin@example.com>
2022-02-24 19:38:12 +0100

    Set correct mkdir path

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ set -e
 # export environment variables
 export $(cat /home/ubuntu/.env | xargs)
 
+mkdir /home/ubuntu/${APP_NAME}
 cd /home/ubuntu/${APP_NAME}
 
 # manage npm dependencies and running
